f6a59b0e480e2d8786504324af8813bb8b68c6eb
Liang Fang <mailfangliang@gmail.com>
2015-04-27 17:07:48 -0400

    add ticketservice

--- src/com/takebus/dao/TicketDao.java
+++ src/com/takebus/dao/TicketDao.java
@@ -5,20 +5,17 @@ import java.util.*;
 import com.takebus.model.Ticket;
 
 public enum TicketDao {
-
 	instance;
 	
 	private Map<String, Ticket> ticketPool = new HashMap<String, Ticket>();
 	
 	private TicketDao() {
-		Ticket t1 = new Ticket(1, 1, 1, 1, "2015-04-21", "roundtrip", "barcode", "yes", 15);
-		Ticket t2 = new Ticket(2, 1, 1, 5, "2015-04-22", "roundtrip", "barcode", "yes", 15);
-		Ticket t3 = new Ticket(3, 1, 1, 1, "2015-05-20", "roundtrip", "barcode", "yes", 15);
+		Ticket t1 = new Ticket(1, 1, 1, "CBT583", "1400 Washington Ave", "7:20am", "888 6th Ave", "11:00am", "Capital Bus Tours", "2015-04-21", "round", "barcode", "yes", 15);
+		Ticket t2 = new Ticket(2, 1, 1, "CBT134", "888 6th Ave", "8:30am", "1400 Washington Ave", "12:00pm", "Capital Bus Tours", "2015-04-22", "round", "barcode", "yes", 15);
 		
-		//Ticket t2 = new Ticket(2, 1, "Dean Brian", "2015-04-26", "", "", 15);
+		Ticket t3 = new Ticket(1, 1, 2, "CBT783", "1400 Washington Ave", "5:50pm", "888 6th Ave", "9:30pm", "Capital Bus Tours", "2015-05-12", "single", "barcode", "no", 15);
+
 		
-		//Ticket t3 = new Ticket(3, 3, "Richard Claderman", "2015-05-01", "", "", 15); 
-		//Ticket t4 = new Ticket(4, 7, "Claderman Claderman", "2015-05-02", "", "", 15); 
 		
 		ticketPool.put("1", t1);
 		ticketPool.put("2", t2);
---
+++ src/com/takebus/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.takebus.service;
+
+import java.util.*;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.takebus.dao.OrderDao;
+import com.takebus.model.Order;
+
+@Path("/order")
+public class OrderService {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Order> getOrders() {
+		List<Order> list = new ArrayList<Order>();
+		list.addAll(OrderDao.instance.getModel().values());
+		return list;
+	}
+	
+	@GET
+	@Path("count")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCount() {
+		int count = OrderDao.instance.getModel().size();
+		return String.valueOf(count);
+	}
+}
