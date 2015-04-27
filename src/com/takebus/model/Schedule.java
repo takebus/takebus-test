73fdbcde85d9bbb478767fe1ffb240a6a3b1e865
QIONG CHEN <qiongchen@QIONGs-MacBook-Pro.local>
2015-04-27 16:51:58 -0700

    modify schedULEsERIVCE

--- src/com/takebus/model/Schedule.java
+++ src/com/takebus/model/Schedule.java
@@ -6,65 +6,56 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Schedule {
 
-	private int scheduleID;
-	private String scheduleIDStr;
-	private int departureCityID;
 	private String departureTime;
-	private int arrivalCityID;
 	private String arrivalTime;
-	private int busOperatorID;
-	private float price;
+	private String operator;
+	private String schedualIDStr;
+	private String price;
+	private String departureStation;
+	private String arrivalStation;
+	private String duration;
+	private String availability;
 	
-	public Schedule() {}
-	
-	public Schedule(int scheduleID, String scheduleIDStr, int departureCityID, String departureTime, int arrivalCityID, String arrivalTime, int busOperatorID, float price) {
-		this.scheduleID = scheduleID;
-		this.scheduleIDStr = scheduleIDStr;
-		this.departureCityID = departureCityID;
+	public Schedule(String departureTime, String arrivalTime, String operator,
+					String schedualIDStr, String price, String departureStation,
+					String arrivalStation, String duration, String availability) {
+		super();
 		this.departureTime = departureTime;
-		this.arrivalCityID = arrivalCityID;
 		this.arrivalTime = arrivalTime;
-		this.busOperatorID = busOperatorID;
+		this.operator = operator;
+		this.schedualIDStr = schedualIDStr;
 		this.price = price;
+		this.departureStation = departureStation;
+		this.arrivalStation = arrivalStation;
+		this.duration = duration;
+		this.availability = availability;
 	}
 	
-	public int getScheduleID() { return scheduleID;}
-	public void setScheduleID(int scheduleID) {
-		this.scheduleID = scheduleID;
+	public String getDepartureTime() {
+		return departureTime;
 	}
-	
-	public String getScheduleIDStr() { return scheduleIDStr;}
-	public void setScheduleIDStr(String scheduleIDStr) {
-		this.scheduleIDStr = scheduleIDStr;
+	public String getArrivalTime() {
+		return arrivalTime;
 	}
-	
-	public int getDepartureCityID() { return departureCityID;}
-	public void setDepartureCityID(int departureCityID) {
-		this.departureCityID = departureCityID;
+	public String getOperator() {
+		return operator;
 	}
-	
-	public String getDepartureTime() { return departureTime;}
-	public void setDepartureTime(String departureTime) {
-		this.departureTime = departureTime;
+	public String getSchedualIDStr() {
+		return schedualIDStr;
 	}
-	
-	public int getArrivalCityID() { return arrivalCityID;}
-	public void setArrivalCityID(int arrivalCityID) {
-		this.arrivalCityID = arrivalCityID;
+	public String getPrice() {
+		return price;
 	}
-	
-	public String getArrivalTime() { return arrivalTime;}
-	public void setArrivalTime(String arrivalTime) {
-		this.arrivalTime = arrivalTime;
+	public String getDepartureStation() {
+		return departureStation;
 	}
-	
-	public int getBusOperatorID() { return busOperatorID;}
-	public void setBusOperatorID(int busOperatorID) {
-		this.busOperatorID = busOperatorID;
+	public String getArrivalStation() {
+		return arrivalStation;
 	}
-	
-	public float getPrice() { return price;}
-	public void setPrice(float price) {
-		this.price = price;
+	public String getDuration() {
+		return duration;
+	}
+	public String getAvailability() {
+		return availability;
 	}
 }
